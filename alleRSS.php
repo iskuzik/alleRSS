b9df474701c7d85f6454c82307a2cd5662a8622d
iskuzik <37713600+iskuzik@users.noreply.github.com>
2018-03-30 01:09:35 +0200

    Update and rename alleRSSsearch.php to alleRSS.php
    Zmiana nazwy pliku.
    Dodanie parametrów: "userId", "categoryId" oraz kilku nowych opcji oferty.
    Poprawienie wyświetlania tytułu i opisu kanału RSS w związku z dodaniem nowych parametrów.

--- alleRSS.php
+++ alleRSS.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ALL_KEY' , 'tutaj wklej klucz webapi allegro'); //klucz webapi allegro 
+define('ALL_KEY' , ' tutaj wklej klucz webapi allegro'); //klucz webapi allegro
 define('RES_SIZE' , 60); //domyślna ilość zwracanych wyników
 
 $filterOptions = array();
@@ -9,8 +9,16 @@ if(isset($_GET['string']) && strlen($_GET['string']) > 1) {
     $searchString = str_replace("+", " ", $_GET['string']);
     $filterOptions[] = array('filterId' => 'search',
     						'filterValueId' => array($searchString));
-} else {
-    die("Parametr \"string\" musi zawierac przynajmniej 2 znaki!");
+}
+
+if(isset($_GET['userId'])) {
+    	$filterOptions[] = array('filterId' => 'userId',
+                        		'filterValueId' => array($_GET['userId']));
+}
+
+if(isset($_GET['categoryId'])) {
+    	$filterOptions[] = array('filterId' => 'category',
+                        		'filterValueId' => array($_GET['categoryId']));
 }
 
 if(isset($_GET['resultSize']) && $_GET['resultSize'] > 1 && $_GET['resultSize'] < 1001) {
@@ -73,11 +81,45 @@ if(isset($_GET['condition'])) {
     }
 }
 
+/* Offer Options */
+
+if(isset($_GET['freeShipping']) && $_GET['freeShipping'] == 1) {
+    
+    	$filterOptions[] = array('filterId' => 'offerOptions',
+                        		'filterValueId' => array('freeShipping'));
+}
+
+if(isset($_GET['freeReturn']) && $_GET['freeReturn'] == 1) {
+    
+    	$filterOptions[] = array('filterId' => 'offerOptions',
+                        		'filterValueId' => array('freeReturn'));
+}
+
+if(isset($_GET['generalDelivery']) && $_GET['generalDelivery'] == 1) {
+    
+    	$filterOptions[] = array('filterId' => 'offerOptions',
+                        		'filterValueId' => array('generalDelivery'));
+}
+
+if(isset($_GET['installmentAvailable']) && $_GET['installmentAvailable'] == 1) {
+    
+    	$filterOptions[] = array('filterId' => 'offerOptions',
+                        		'filterValueId' => array('installmentAvailable'));
+}
+
+if(isset($_GET['vatInvoice']) && $_GET['vatInvoice'] == 1) {
+    
+    	$filterOptions[] = array('filterId' => 'offerOptions',
+                        		'filterValueId' => array('vatInvoice'));
+}
+
 if(isset($_GET['personalReceipt']) && $_GET['personalReceipt'] == 1) {
     
     	$filterOptions[] = array('filterId' => 'offerOptions',
                         		'filterValueId' => array('personalReceipt'));
 }
+
+/* Location Options */
 
 if(isset($_GET['city'])) {
 	$city = str_replace("+", " ", $_GET['city']);
@@ -109,57 +151,98 @@ try {
 															'sortOrder' => 'desc'),
 									'resultSize' => $resultSize,
 									'resultOffset' => 0,
-									'resultScope' => 3);
+									'resultScope' => 1);
  
 	try {
 		$response = $client->doGetItemsList($doGetItemsList_request);
+		
+		if (isset($_GET['string'])) {
+				$infoString = "Wyszukiwanie: " . $searchString;
+		}
+		
+		if (isset($_GET['userId'])) {
+			if (isset($_GET['string'])) {
+         		$infoString .= ", użytkownik: ";
+         	} else {
+         		$infoString = "Użytkownik: ";
+         	}
+         	
+         	if ($response->itemsCount > 1) {
+         		$infoString .= $response->itemsList->item[0]->sellerInfo->userLogin;
+         	} else {
+         		$infoString .= $_GET['userId'];
+         	}
+         }
+         
+         if (isset($_GET['categoryId'])) {
+         	if (isset($_GET['string']) || isset($_GET['userId'])) {
+         		$infoString .= ", kategoria: ";
+         	} else {
+         		$infoString = "Kategoria: ";
+         	}
+         	
+         	if ($response->itemsCount > 1) {
+         	
+         		foreach ($response->categoriesList->categoriesTree->item as $key => $cat) {
+         			if($cat->categoryId == $_GET['categoryId']) {
+         				$infoString .= $cat->categoryName;
+         			}
+         		}
+         	} else {
+         		$infoString .= $_GET['categoryId'];
+         	}
+         }
         
         echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
         echo "<rss version=\"2.0\">\n";
         echo "<channel>\n";
-        echo "<title>Allegro.pl - $searchString</title>\n";
+        echo "<title>Allegro.pl - $infoString</title>\n";
         echo "<link>https://allegro.pl</link>\n";
-        echo "<description>$searchString - najnowsze oferty. Promowane: ". $response->itemsFeaturedCount . "/" . ($response->itemsCount > $resultSize ? $resultSize : $response->itemsCount) . "</description>\n";
+        echo "<description>$infoString - najnowsze oferty. Promowane: ". $response->itemsFeaturedCount . "/" . ($response->itemsCount > $resultSize ? $resultSize : $response->itemsCount) . "</description>\n";
         
 		foreach ($response->itemsList->item as $key => $object) {
 			echo "<item>\n";
 			echo "<title>" . $object->itemTitle . "</title>\n";
 			echo "<link>https://allegro.pl/i" . $object->itemId . ".html</link>\n";
 			echo "<description>\n";
-			echo "<![CDATA[Sprzedający: <a href=\"https://allegro.pl/show_user.php?uid=" . $object->sellerInfo->userId . "\">" . $object->sellerInfo->userLogin . "</a><br />]]>\n";
-			echo "<![CDATA[Do końca: " . $object->timeToEnd . ($object->endingTime == "" ? "" : " (") . str_replace("T", ", ", $object->endingTime) . ($object->endingTime == "" ? "" : ") ") . "<br />]]>\n";
+			echo "<![CDATA[Sprzedający: <a href=\"https://allegro.pl/show_user.php?uid=" . $object->sellerInfo->userId . "\">" . $object->sellerInfo->userLogin . "</a><br />\n";
+			echo "Do końca: " . $object->timeToEnd;
+			
+			if (isSet($object->endingTime)) {
+				echo " (";
+				echo str_replace("T", ", ", $object->endingTime);
+				echo ")";
+			}
+			echo "<br />\n";
 			
 			foreach ($object->priceInfo->item as $key => $price) {
 				if ($price->priceType == "bidding") {
-					echo "<![CDATA[Aktualna cena: " . $price->priceValue . " zł<br />]]>\n";
+					echo "Aktualna cena: " . $price->priceValue . "zł<br />\n";
 				}
 				
 				if ($price->priceType == "buyNow") {
 				
-					echo "<![CDATA[Cena Kup Teraz: " . $price->priceValue . " zł<br />]]>\n";
+					echo "Cena Kup Teraz: " . $price->priceValue . "zł<br />\n";
 				}
 			}
 			
-			echo "<![CDATA[<p><a href=\"https://allegro.pl/i" . $object->itemId . ".html\">Przejdź do oferty</a></p>]]>\n";
+			echo "<p><a href=\"https://allegro.pl/i" . $object->itemId . ".html\">Przejdź do oferty</a></p>\n";
 			
-			if($object->photosInfo->item) {
+			if(isSet($object->photosInfo->item)) {
 				foreach ($object->photosInfo->item as $key => $photo) {
 			
 					if ($photo->photoSize == "medium" && $photo->photoIsMain == 1) {
- 		   				echo "<![CDATA[<a href=\"https://allegro.pl/i" . $object->itemId . ".html\"><img src=\"" . $photo->photoUrl . "\"></a>]]>\n";
+ 		   				echo "<a href=\"https://allegro.pl/i" . $object->itemId . ".html\"><img src=\"" . $photo->photoUrl . "\"></a>]]>\n";
  		   			}
 				}
 			}
 			echo "</description>\n";
-			
 			echo "<guid isPermaLink=\"false\">" . $object->itemId . "</guid>\n";
 			echo "</item>\n";
 
 		}
-
     	echo "</channel>\n";
     	echo "</rss>";
-        
 	}
     catch(SoapFault $error) {
         echo $error->faultstring;
@@ -168,7 +251,7 @@ try {
 }
 
 catch(SoapFault $error) {
-	echo 'Błąd ', $error->faultcode, ': ', $error->faultstring, "n";
+	echo 'Błąd ', $error->faultcode, ': ', $error->faultstring;
 }
 
 ?>
